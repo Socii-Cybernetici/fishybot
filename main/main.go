8cde63d3c7f45d1934cfc386f424a38e2f62899b
s7electric <samiacobazzi@gmail.com>
2026-07-22 23:04:08 -0400

    initial merge from local

---
+++ main/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+const nora_user_id string = "976576169325522994"
+const threespeed_user_id string = "489166470589448220"
+const phxie_user_id string = "362297684742373386"
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	var BOT_PORT string = os.Getenv("BOT_PORT")
+	var BOT_TOKEN string = os.Getenv("DISCORD_BOT_TOKEN")
+
+	discord_session, err := discordgo.New("Bot " + BOT_TOKEN)
+	if err != nil {
+		panic(err)
+	}
+
+	/* Server Init */
+	var srv_handler *http.ServeMux = http.NewServeMux()
+	var srv http.Server = http.Server{
+		Addr:    ":" + BOT_PORT,
+		Handler: srv_handler,
+	}
+	srv_handler.HandleFunc("GET /", func(wr http.ResponseWriter, rq *http.Request) {
+		wr.WriteHeader(200)
+		wr.Write([]byte("nothing to see here; use: POST /submit\n"))
+		log.Print("Received GET /\n")
+	})
+	srv_handler.HandleFunc("POST /submit", func(wr http.ResponseWriter, rq *http.Request) {
+		var p []byte
+		_, err := rq.Body.Read(p)
+		if err != nil {
+			wr.WriteHeader(500)
+
+		}
+
+		ch, err := discord_session.UserChannelCreate(threespeed_user_id)
+		if err != nil {
+			wr.WriteHeader(500)
+			wr.Write([]byte("Failed to reach discord API; " + err.Error()))
+			log.Print("Could not reach discord api for this request\n")
+			return
+		}
+		_, err = discord_session.ChannelMessageSend(ch.ID, "Submission Received:\n"+string(p))
+		if err != nil {
+			wr.WriteHeader(500)
+			wr.Write([]byte("Failed to reach discord API; " + err.Error()))
+			log.Print("Could not reach discord api for this request\n")
+			return
+		}
+		wr.WriteHeader(200)
+		wr.Write([]byte("Submission received:\n" + string(p)))
+		log.Print("Submission received:\n" + string(p))
+	})
+	log.Printf("Listening on port %s", BOT_PORT)
+	err = srv.ListenAndServe()
+	log.Fatal(err.Error())
+}
